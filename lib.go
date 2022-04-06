50fc0d69b5ebb98f41833cd275f2312982468cbb
Iljasov Ramil <dravenshadowkeeper@gmail.com>
2022-04-06 14:29:28 +0500

    source

---
+++ lib.go
@@ -0,0 +1,357 @@
+package refcntmemcache
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var ErrRefIsNotZero = errors.New("reference counter is not zero")
+var ErrEmptyHandle = errors.New("handle is nil")
+
+type command int
+
+const (
+	stopProcessDeletedValues     command = 1
+	listOfDeletedValuesIsChanged command = 2
+)
+
+// Без этого интерфейса придётся или тащить Key в Handle или Mutex в каждый cacheDatum
+type keylessRefCntMemCache[Value any] interface {
+	put(handle *Handle[Value])
+}
+
+type listLinks[Value any] struct {
+	next *cacheDatum[Value]
+	prev *cacheDatum[Value]
+}
+
+type cacheDatum[Value any] struct {
+	counter        uint64
+	key            interface{}
+	valuesToDelete listLinks[Value] // Список элементов которые можно удалять. Упорядочено по time, если lifetime > 0.
+	time           time.Time
+	value          *Value
+}
+
+// Извлечь из списка
+func (cd *cacheDatum[Value]) extractFromList(first, last **cacheDatum[Value]) {
+	if cd.valuesToDelete.prev != nil {
+		cd.valuesToDelete.prev.valuesToDelete.next = cd.valuesToDelete.next
+	}
+	if cd.valuesToDelete.next != nil {
+		cd.valuesToDelete.next.valuesToDelete.prev = cd.valuesToDelete.prev
+	}
+
+	if *first == cd {
+		*first = cd.valuesToDelete.next
+	}
+	if *last == cd {
+		*last = cd.valuesToDelete.prev
+	}
+
+	cd.valuesToDelete.next = nil
+	cd.valuesToDelete.prev = nil
+}
+
+// Добавить в список. sortByTime чтобы time было упорядочено по возрастанию.
+func (cd *cacheDatum[Value]) insertToList(first, last **cacheDatum[Value], sortByTime bool) {
+	if *first != nil {
+		if !sortByTime || (*first).time.After(cd.time) {
+			cd.valuesToDelete.next = *first
+			cd.valuesToDelete.prev = nil
+		} else {
+			// Найдём последний p у которого время меньше чем у cd
+			p := *first
+			for p.valuesToDelete.next != nil {
+				if p.valuesToDelete.next.time.After(cd.time) {
+					break
+				}
+				p = p.valuesToDelete.next
+			}
+
+			// Вставим cd после p
+			cd.valuesToDelete.prev = p
+			if p.valuesToDelete.next != nil {
+				p.valuesToDelete.next.valuesToDelete.prev = cd
+			}
+
+			cd.valuesToDelete.next = p.valuesToDelete.next
+			p.valuesToDelete.next = cd
+		}
+	} else {
+		cd.valuesToDelete.prev = nil
+		cd.valuesToDelete.next = nil
+	}
+
+	if *last == cd.valuesToDelete.prev {
+		*last = cd
+	}
+
+	if *first == cd.valuesToDelete.next {
+		*first = cd
+	}
+}
+
+type RefCntMemCache[Key comparable, Value any] struct {
+	lifetime    time.Duration
+	maxElements int
+
+	commands     chan command
+	waitStopRcmc chan struct{}
+
+	// Область защищённая мьютексом
+	mut            sync.Mutex
+	values         map[Key]*cacheDatum[Value]
+	valuesToDelete struct {
+		first *cacheDatum[Value]
+		last  *cacheDatum[Value]
+	} // Список элементов которые можно удалять. Упорядочено по time.
+	freeFunc func(value *Value)
+}
+
+type noCopy struct{}
+
+func (*noCopy) Lock() {}
+
+func (*noCopy) Unlock() {}
+
+// Handle и его методы безопасно использовать только из одной нити.
+type Handle[Value any] struct {
+	noCopy
+	keylessRefCntMemCache keylessRefCntMemCache[Value]
+	cacheDatum            *cacheDatum[Value]
+	value                 *Value
+}
+
+func (handle *Handle[Value]) Get() *Value {
+	return handle.value
+}
+
+func (handle *Handle[Value]) Put() {
+	handle.keylessRefCntMemCache.put(handle)
+}
+
+func New[Key comparable, Value any](lifetime time.Duration, maxElements int) *RefCntMemCache[Key, Value] {
+	rcmc := &RefCntMemCache[Key, Value]{lifetime: lifetime, maxElements: maxElements}
+	rcmc.commands = make(chan command, 1)
+	rcmc.waitStopRcmc = make(chan struct{})
+	rcmc.values = make(map[Key]*cacheDatum[Value])
+
+	deleteOldValues := func() {
+		timer := time.NewTimer(24 * time.Hour)
+
+		for {
+			duration := func() (duration time.Duration) {
+				rcmc.mut.Lock()
+				defer rcmc.mut.Unlock()
+
+				if lifetime > 0 && rcmc.valuesToDelete.first != nil {
+					duration = rcmc.valuesToDelete.first.time.Add(lifetime).Sub(time.Now())
+					if duration < 0 {
+						duration = time.Duration(0)
+					}
+				} else {
+					duration = 24 * time.Hour // Большое время без всякого смысла
+				}
+
+				return
+			}()
+
+			timer.Stop()
+			timer.Reset(duration)
+
+			select {
+			case cmd := <-rcmc.commands:
+				if cmd == stopProcessDeletedValues {
+					rcmc.waitStopRcmc <- struct{}{}
+					break
+				} else if cmd == listOfDeletedValuesIsChanged {
+					// empty
+				}
+			case <-timer.C:
+				func() {
+					rcmc.mut.Lock()
+					defer rcmc.mut.Unlock()
+					rcmc.freeValuesToDelete(true, false)
+				}()
+			}
+		}
+	}
+
+	if lifetime > 0 {
+		go deleteOldValues()
+	}
+
+	return rcmc
+}
+
+func (rcmc *RefCntMemCache[Key, Value]) Close() error {
+	err := func() error {
+		rcmc.mut.Lock()
+		defer rcmc.mut.Unlock()
+
+		for _, cacheDatum := range rcmc.values {
+			if cacheDatum.counter > 0 {
+				return ErrRefIsNotZero
+			}
+		}
+
+		rcmc.values = make(map[Key]*cacheDatum[Value])
+
+		return nil
+	}()
+	if err != nil {
+		return err
+	}
+
+	if rcmc.lifetime > 0 {
+		rcmc.commands <- stopProcessDeletedValues
+		<-rcmc.waitStopRcmc
+	}
+
+	return nil
+}
+
+func (rcmc *RefCntMemCache[Key, Value]) SetFree(free func(value *Value)) {
+	rcmc.mut.Lock()
+	defer rcmc.mut.Unlock()
+
+	rcmc.freeFunc = free
+}
+
+// Не thread-safe. Удаляет старые элементы и те, что свехр нормы.
+func (rcmc *RefCntMemCache[Key, Value]) freeValuesToDelete(byTime, byMaxElements bool) {
+	removeFirst := func(cacheDatum *cacheDatum[Value]) {
+		cacheDatum.extractFromList(&rcmc.valuesToDelete.first, &rcmc.valuesToDelete.last)
+
+		cacheDatumValue := cacheDatum.value
+		cacheDatum.value = nil
+
+		key := cacheDatum.key.(Key)
+		delete(rcmc.values, key)
+
+		if rcmc.freeFunc != nil {
+			func() {
+				rcmc.mut.Unlock()
+				defer rcmc.mut.Lock()
+				rcmc.freeFunc(cacheDatumValue)
+			}()
+		}
+	}
+
+	if byTime && rcmc.lifetime > 0 {
+		for rcmc.valuesToDelete.first != nil {
+			cacheDatum := rcmc.valuesToDelete.first
+			if cacheDatum.time.Add(rcmc.lifetime).Before(time.Now()) {
+				removeFirst(cacheDatum)
+			}
+		}
+	}
+
+	if byMaxElements && rcmc.maxElements > 0 {
+		if len(rcmc.values) > rcmc.maxElements {
+			remove := len(rcmc.values) - rcmc.maxElements
+			for remove > 0 && rcmc.valuesToDelete.first != nil {
+				cacheDatum := rcmc.valuesToDelete.first
+				removeFirst(cacheDatum)
+				remove--
+			}
+		}
+	}
+}
+
+func (rcmc *RefCntMemCache[Key, Value]) put(handle *Handle[Value]) {
+	isListChanged := func() bool {
+		rcmc.mut.Lock()
+		defer rcmc.mut.Unlock()
+
+		cacheDatum := handle.cacheDatum
+		handle.cacheDatum = nil
+		handle.value = nil
+
+		if cacheDatum == nil || cacheDatum.counter == 0 {
+			return false
+		}
+
+		cacheDatum.counter--
+
+		if cacheDatum.counter > 0 {
+			return false
+		}
+
+		cacheDatum.insertToList(&rcmc.valuesToDelete.first, &rcmc.valuesToDelete.last, rcmc.lifetime > 0)
+
+		rcmc.freeValuesToDelete(false, true)
+
+		return true
+	}()
+
+	if rcmc.lifetime > 0 && isListChanged && len(rcmc.commands) == 0 {
+		rcmc.commands <- listOfDeletedValuesIsChanged
+	}
+
+	return
+}
+
+func (rcmc *RefCntMemCache[Key, Value]) Get(key Key) (handle Handle[Value]) {
+	handle = Handle[Value]{noCopy{}, rcmc, nil, nil}
+
+	isListChanged := func() bool {
+		rcmc.mut.Lock()
+		defer rcmc.mut.Unlock()
+
+		cacheDatum, exists := rcmc.values[key]
+		if !exists {
+			return false
+		}
+
+		handle.cacheDatum = cacheDatum
+		handle.value = cacheDatum.value
+
+		cacheDatum.counter++
+
+		// Обновляем время доступа и убираем из списка на удаление
+		cacheDatum.time = time.Now()
+		cacheDatum.extractFromList(&rcmc.valuesToDelete.first, &rcmc.valuesToDelete.last)
+
+		return true
+	}()
+
+	if rcmc.lifetime > 0 && isListChanged && len(rcmc.commands) == 0 {
+		rcmc.commands <- listOfDeletedValuesIsChanged
+	}
+
+	return
+}
+
+func (rcmc *RefCntMemCache[Key, Value]) Set(key Key, value *Value) (handle Handle[Value], new bool) {
+	func() {
+		rcmc.mut.Lock()
+		defer rcmc.mut.Unlock()
+
+		datum, exists := rcmc.values[key]
+		if !exists {
+			datum = &cacheDatum[Value]{0, key, listLinks[Value]{}, time.Time{}, value}
+			rcmc.values[key] = datum
+			new = true
+		}
+
+		handle = Handle[Value]{noCopy{}, rcmc, datum, datum.value}
+		datum.counter++
+
+		// Обновляем время доступа и убираем из списка на удаление
+		datum.time = time.Now()
+		datum.extractFromList(&rcmc.valuesToDelete.first, &rcmc.valuesToDelete.last)
+
+		rcmc.freeValuesToDelete(false, true)
+	}()
+
+	if rcmc.lifetime > 0 && len(rcmc.commands) == 0 {
+		rcmc.commands <- listOfDeletedValuesIsChanged
+	}
+
+	return
+}
+
+// TODO: надо бы заменить list для valuesToDelete на дерево поиска.
---
+++ lib_test.go
@@ -0,0 +1,282 @@
+package refcntmemcache_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/mdraven/refcntmemcache"
+	"github.com/stretchr/testify/assert"
+)
+
+type testType struct {
+	value string
+}
+
+func TestBasicSetGetPut(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Second, 10)
+
+	handle1, new := cache.Set(1, &testType{"value1"})
+	assert.True(t, new)
+	assert.Equal(t, "value1", handle1.Get().value)
+
+	handle1.Put()
+	assert.Nil(t, cache.Close())
+}
+
+func TestCloseWithoutPut(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Second, 10)
+
+	cache.Set(1, &testType{"value1"})
+
+	assert.NotNil(t, cache.Close())
+}
+
+func TestDoublePut(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Second, 10)
+
+	handle1, _ := cache.Set(1, &testType{"value1"})
+
+	handle1.Put()
+	handle1.Put()
+	assert.Nil(t, cache.Close())
+}
+
+func TestGetAfterPut(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Second, 10)
+
+	handle1, _ := cache.Set(1, &testType{"value1"})
+
+	handle1.Put()
+
+	assert.Nil(t, handle1.Get())
+}
+
+func TestDoubleSet(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Second, 10)
+
+	data := &testType{"value1"}
+
+	handle1, new := cache.Set(1, data)
+	assert.True(t, new)
+
+	handle2, new := cache.Set(1, &testType{"value2"})
+	assert.False(t, new)
+
+	assert.Equal(t, data, handle1.Get())
+	assert.Equal(t, data, handle2.Get())
+}
+
+func TestRemoveByDuration(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	data := &testType{"value1"}
+
+	handle1, _ := cache.Set(1, data)
+	assert.NotNil(t, handle1.Get())
+	handle1.Put()
+
+	time.Sleep(time.Millisecond * 200)
+
+	handle2 := cache.Get(1)
+	assert.Nil(t, handle2.Get())
+}
+
+func TestRemoveByMaxElements(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Duration(0), 2)
+
+	data := &testType{"value1"}
+
+	handle1, _ := cache.Set(1, data)
+	assert.NotNil(t, handle1.Get())
+
+	handle2, _ := cache.Set(2, data)
+	assert.NotNil(t, handle2.Get())
+
+	handle2.Put()
+	handle1.Put()
+
+	handle3, _ := cache.Set(3, data)
+	assert.NotNil(t, handle3.Get())
+
+	handle4 := cache.Get(1)
+	assert.Nil(t, handle4.Get())
+
+	handle5 := cache.Get(2)
+	assert.NotNil(t, handle5.Get())
+}
+
+func TestFreeFunc(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	free := make(chan string, 1)
+
+	cache.SetFree(func(value *testType) {
+		free <- value.value
+	})
+
+	data := &testType{"value1"}
+
+	handle1, _ := cache.Set(1, data)
+	assert.NotNil(t, handle1.Get())
+	handle1.Put()
+
+	time.Sleep(time.Millisecond * 200)
+
+	assert.Equal(t, data.value, <-free)
+}
+
+func TestFreeHandleInFreeFun(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	data := &testType{"value1"}
+
+	free := make(chan *testType)
+	cache.SetFree(func(value *testType) {
+		handle3 := cache.Get(2)
+		free <- handle3.Get()
+		handle3.Put()
+	})
+
+	handle1, _ := cache.Set(1, data)
+	handle2, _ := cache.Set(2, data)
+
+	handle1.Put()
+
+	time.Sleep(time.Millisecond * 200)
+
+	assert.Equal(t, data, <-free)
+
+	handle2.Put()
+
+	time.Sleep(time.Millisecond * 200)
+
+	assert.Nil(t, <-free)
+}
+
+func TestCheckingCounter(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	data := &testType{"value1"}
+
+	handle1, _ := cache.Set(1, data)
+	assert.NotNil(t, handle1.Get())
+
+	time.Sleep(time.Millisecond * 300)
+
+	handle2 := cache.Get(1)
+	assert.NotNil(t, handle2.Get())
+}
+
+func TestRemoveOrderByTime1(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	free := make(chan string, 10)
+	cache.SetFree(func(value *testType) {
+		free <- value.value
+	})
+
+	handle1, _ := cache.Set(1, &testType{"value1"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle1 и handle2 отличались
+	handle2, _ := cache.Set(2, &testType{"value2"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle2 и handle3 отличались
+	handle3, _ := cache.Set(3, &testType{"value3"})
+
+	handle2.Put()
+	handle1.Put()
+	handle3.Put()
+
+	assert.Equal(t, "value1", <-free)
+	assert.Equal(t, "value2", <-free)
+	assert.Equal(t, "value3", <-free)
+}
+
+func TestRemoveOrderByTime2(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Millisecond*100, 10)
+
+	free := make(chan string, 10)
+	cache.SetFree(func(value *testType) {
+		free <- value.value
+	})
+
+	handle1, _ := cache.Set(1, &testType{"value1"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle1 и handle2 отличались
+	handle2, _ := cache.Set(2, &testType{"value2"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle2 и handle3 отличались
+	handle3, _ := cache.Set(3, &testType{"value3"})
+
+	handle3.Put()
+	handle1.Put()
+	handle2.Put()
+
+	assert.Equal(t, "value1", <-free)
+	assert.Equal(t, "value2", <-free)
+	assert.Equal(t, "value3", <-free)
+}
+
+func TestRemoveOrderByMaxElements(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Duration(0), 1)
+
+	free := make(chan string, 10)
+	cache.SetFree(func(value *testType) {
+		free <- value.value
+	})
+
+	cache.Set(333, &testType{""}) // Этот элемент нужен чтобы занимать место в cache
+
+	handle1, _ := cache.Set(1, &testType{"value1"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle1 и handle2 отличались
+	handle2, _ := cache.Set(2, &testType{"value2"})
+	time.Sleep(time.Millisecond * 10) // чтобы time у handle2 и handle3 отличались
+	handle3, _ := cache.Set(3, &testType{"value3"})
+
+	handle2.Put()
+	handle1.Put()
+	handle3.Put()
+
+	assert.Equal(t, "value2", <-free)
+	assert.Equal(t, "value1", <-free)
+	assert.Equal(t, "value3", <-free)
+}
+
+func TestThreadSafeTest(t *testing.T) {
+	cache := refcntmemcache.New[int, testType](time.Duration(0), 1)
+
+	start := sync.WaitGroup{}
+	result := make(chan bool)
+	wait := sync.WaitGroup{}
+
+	numGoroutines := 4
+	numValues := 100
+
+	start.Add(1)
+	for i := 0; i < numGoroutines; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+
+			start.Wait()
+
+			for i := 0; i < numValues; i++ {
+				value := fmt.Sprintf("value%d", i)
+
+				handle1, _ := cache.Set(i, &testType{value})
+				defer handle1.Put()
+
+				handle2 := cache.Get(i)
+				defer handle2.Put()
+				result <- handle2.Get().value == value
+			}
+		}()
+	}
+
+	start.Done()
+	for i := 0; i < numGoroutines*numValues; i++ {
+		assert.True(t, <-result)
+	}
+
+	wait.Wait()
+
+	assert.Nil(t, cache.Close())
+}
